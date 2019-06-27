30c2120f2e1b44fddbe9eee5300dc5b7e51b6ae4
Ernest Hsu <xuyn1980@gmail.com>
2019-06-27 17:55:01 +0800

    (*) adjust wx oauth callback url

--- src/main/java/net/ltsoftware/usercenter/constant/WxLoginConstants.java
+++ src/main/java/net/ltsoftware/usercenter/constant/WxLoginConstants.java
@@ -4,7 +4,7 @@ public interface WxLoginConstants {
 
     String CODE_API = "https://open.weixin.qq.com/connect/qrconnect";
     String APPID = "wxe6624d74e53bd06c";
-    String REDIRECT_URI = "http://uc.ltsoftware.net/oauth/wxcallback";
+    String REDIRECT_URI = "https://uc.ltsoftware.net/oauth/wxcallback";
     String RESPONSE_TYPE = "code";
     String SCOPE = "snsapi_login";
 
