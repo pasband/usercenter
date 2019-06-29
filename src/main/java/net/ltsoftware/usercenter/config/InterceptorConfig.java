7e663627255dd1f081fd9ae197dcb118cdbad09a
Ernest Hsu <xuyn1980@gmail.com>
2019-06-29 23:03:23 +0800

    (*) fix interceptor autowire null

--- src/main/java/net/ltsoftware/usercenter/config/InterceptorConfig.java
+++ src/main/java/net/ltsoftware/usercenter/config/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package net.ltsoftware.usercenter.config;
 
 import net.ltsoftware.usercenter.interceptor.AuthenticationInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,5 +13,15 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthenticationInterceptor()).addPathPatterns("/**");
 
+//        InterceptorRegistration registration = registry.addInterceptor(new LoginInterceptor());
+//        registration.addPathPatterns("/**");                    //所有路径都被拦截
+//        registration.excludePathPatterns("/","/login","/error","/static/**","/qwe/**");       //添加不拦截路径
+
     }
+
+    @Bean
+    public AuthenticationInterceptor authenticationInterceptor(){
+        return new AuthenticationInterceptor();
+    }
+
 }
