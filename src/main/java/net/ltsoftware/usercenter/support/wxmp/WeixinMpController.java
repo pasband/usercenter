3ebe0f9a3a3837390a56ee63e0b4088f07d9c7c7
Ernest Hsu <xuyn1980@gmail.com>
2019-07-12 01:44:31 +0800

    （+）add swagger support

--- src/main/java/net/ltsoftware/usercenter/support/wxmp/WeixinMpController.java
+++ src/main/java/net/ltsoftware/usercenter/support/wxmp/WeixinMpController.java
@@ -1,6 +1,10 @@
 package net.ltsoftware.usercenter.support.wxmp;
 
 import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import net.ltsoftware.usercenter.constant.ErrorCode;
 import net.ltsoftware.usercenter.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Api(value = "WeixinMpController")
 @Controller
 public class WeixinMpController {
 
     @Autowired
     private WeixinMpService weixinMpService;
 
+    @ApiOperation(value = "获取签名信息")
+    @ApiImplicitParams({@ApiImplicitParam(name = "url", value = "网页链接", required = true, dataType = "String")})
     @GetMapping("/wxmp/sign")
     public void getWxmpSign(String url, HttpServletResponse response){
         JSONObject json = weixinMpService.getWxmpSignData(url);
