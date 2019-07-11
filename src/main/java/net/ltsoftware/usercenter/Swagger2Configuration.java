4c49533a0620cbb5a895c11f01c85f398e938432
Ernest Hsu <xuyn1980@gmail.com>
2019-07-12 01:49:43 +0800

    （+）add swagger config

---
+++ src/main/java/net/ltsoftware/usercenter/Swagger2Configuration.java
@@ -0,0 +1,37 @@
+package net.ltsoftware.usercenter;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("旅通软件平台接口文档")
+                .description("用户中心相关接口的文档")
+                .termsOfServiceUrl("https://uc.ltsoftware.net")
+                .version("1.0")
+                .build();
+    }
+
+}
