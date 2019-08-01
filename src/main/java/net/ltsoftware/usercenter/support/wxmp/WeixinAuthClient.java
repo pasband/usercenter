0e990901436df26c5b08e5cfaa1e12b6376e3a93
Ernest Hsu <xuyn1980@gmail.com>
2019-08-01 14:23:39 +0800

    (*) test get openid api 3

--- src/main/java/net/ltsoftware/usercenter/support/wxmp/WeixinAuthClient.java
+++ src/main/java/net/ltsoftware/usercenter/support/wxmp/WeixinAuthClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="wxmp-service",url="https://open.weixin.qq.com/")
+@FeignClient(name="wxauth-service",url="https://open.weixin.qq.com/")
 public interface WeixinAuthClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/connect/oauth2/authorize")
